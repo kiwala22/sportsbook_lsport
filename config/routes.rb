495d05ba1a98da6b442ac8347ef9358d4cd1783f
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-04-21 13:13:03 +0300

    changed routes from subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
 
-  resources :deposits, only: [:index]
-  resources :withdraws, only: [:index]
+   resources :deposits, only: [:index]
+   resources :withdraws, only: [:index]
    devise_for :users, path: 'users',  controllers: {
       sessions: 'users/sessions'
    }
-   constraints subdomain: 'intnet' do
-      devise_for :admins, path: 'admins',  controllers:{
-         sessions: 'admins/sessions'
-      }
-      devise_scope :admin do
+   devise_for :admins, path: 'admins',  controllers:{
+      sessions: 'admins/sessions'
+   }
+   devise_scope :admin do
 
-         authenticated :admin do
-           root :to => 'admin_landing#index', as: :authenticated_root
-         end
-         unauthenticated :admin do
-           get '/', to: "admins/sessions#new"
-         end
+      authenticated :admin do
+         root :to => 'admin_landing#index', as: :authenticated_root
       end
-    end
+      unauthenticated :admin do
+         get '/', to: "admins/sessions#new"
+      end
+   end
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
