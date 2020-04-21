fcef7de980bae47894db98f198a9c22caf514465
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-04-22 02:57:24 +0300

    added node modules to linked

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :passenger_restart_with_touch, true
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env', 'config/initializers/sidekiq.rb')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/reports')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/reports', 'node_modules')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
