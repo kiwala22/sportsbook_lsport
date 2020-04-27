3b9f3dcff32bde56400bb4b3ab05e42f8e8e1778
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-04-28 01:12:57 +0300

    changed fromrvm to rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,7 +53,7 @@ set :rbenv_type, :user # or :system, depends on your rbenv setup
 
 # in case you want to set ruby version from the file:
 # set :rbenv_ruby, File.read('.ruby-version').strip
-set :rbenv_path, '/root/.rbenv/shims/ruby'
+set :rbenv_path, '$HOME/.rbenv/shims/ruby'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
