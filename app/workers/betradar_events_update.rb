7c886051e788099614790a239d51090f61e96f5b
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-04-30 03:05:45 +0300

    changed configs to worker

--- app/workers/betradar_events_update.rb
+++ app/workers/betradar_events_update.rb
@@ -1,8 +1,22 @@
 class BetradarEventsUpdate
    include Sneakers::Worker
    QUEUE_NAME = :update_events
-   from_queue QUEUE_NAME
-   #routing_key: 'ended',
+   from_queue QUEUE_NAME,
+      :heartbeat => 5,
+      :amqp => 'amqp://ANRL2tQf8N40oGQ4Ye:guest@mq.betradar.com:5672',
+      :vhost => '/unifiedfeed/30819',
+      :exchange => 'unifiedfeed',
+      :exchange_type => :direct,
+      :daemonize => true,
+      :workers => 10,
+      :log  => 'sneakers.log',
+      :pid_path => 'sneakers.pid',
+      :prefetch => 10,
+      :threads => 10,
+      :env => ENV['RACK_ENV'],
+      :durable => true,
+      :ack => true
+      #routing_key: 'ended',
 
    def work(message)
       response = JSON.parse(message, symbolize_names: true)
