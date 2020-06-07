f92b0406018c3375d1224fe98ede5b2040e6bd64
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-06-07 13:17:03 +0300

    improved API response

--- app/workers/alerts_worker.rb
+++ app/workers/alerts_worker.rb
@@ -17,7 +17,7 @@ class AlertsWorker
         subscribed  = message_hash["alive"]["subscribed"]
 
         #check the market alert
-        last_update = Marketalert.where(:product => product).order("timestamp DESC").first
+        last_update = MarketAlert.where(:product => product).order("timestamp DESC").first
         if last_update == nil
             #then create it
             last_update = MarketAlert.create(product: product, timestamp:  timestamp, subscribed:  subscribed, status:  recovery_status)
