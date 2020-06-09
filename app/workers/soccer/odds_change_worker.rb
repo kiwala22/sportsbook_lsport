f19833f0ef027bef62498163cc437906f1214008
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-06-09 03:29:23 +0300

    made changes to odd_change

--- app/workers/soccer/odds_change_worker.rb
+++ app/workers/soccer/odds_change_worker.rb
@@ -32,30 +32,38 @@ class Soccer::OddsChangeWorker
         event_id = message["odds_change"]["event_id"]
         product = message["odds_change"]["product"]
         match_status = message["odds_change"]["sport_event_status"]["match_status"]
-        home_score = message["odds_change"]["sport_event_status"]
-        away_score = message["odds_change"]["sport_event_status"]
+        home_score = nil
+        away_score = nil
+
+        if message["odds_change"]["sport_event_status"].has_key?('home_score')
+            home_score = ["odds_change"]["sport_event_status"]["home_score"]
+        end
+
+        if message["odds_change"]["sport_event_status"].has_key?('away_score')
+            away_score = ["odds_change"]["sport_event_status"]["away_score"]
+        end
+
         
         #find the fixture and update the fixture
-        fixture = SoccerFixture.where(event_id: event_id).order(:id).last
+        fixture = SoccerFixture.where(event_id: event_id).order("created_at DESC").first
         if fixture
             fixture.update(home_score: home_score, away_score: away_score, match_status: soccer_status[match_status])
         end
         
         #extract fixture information and update the fixtures with score and match status
-        if message["odds_change"]["sport_event_status"].has_key?("odds")
-            message["odds_change"]["sport_event_status"]["odds"].each do |market|
+        if message["odds_change"].has_key?("odds")
+            message["odds_change"]["odds"]["market"].each do |market|
                 
                 producer_type = {
                     "1" => "Live",
                     "3" => "Pre"
                 }
-                
-                table_sym = "Market" + market["id"] + "_" + producer_type[product]
-                # table_sym = table_name.to_sym
+
+                model_name = "Market" + market["id"]+ producer_type[product]
                 
                 #hard code market with similar outcomes
-                if market["id"] == "1" || "60"
-                    market.each do |out|
+                if market["id"] == "1" || market["id"] == "60"
+                    market["outcome"].each do |out|
                         if out["id"] == "1"
                             comp1_odds = out["odds"]
                         end
@@ -67,7 +75,7 @@ class Soccer::OddsChangeWorker
                         end
                     end
                     #update or create markets 1X2 half time and fulltime
-                    table_sym.update_or_create(event_id: event_id)  do |mkt|
+                    model_name.constantize.find_or_create_by(event_id: event_id)  do |mkt|
                         mkt.competitor1 =  comp1_odds
                         mkt.competitor2 = comp2_odds
                         mkt.draw =  draw_odds
@@ -78,7 +86,7 @@ class Soccer::OddsChangeWorker
                     end
                 end
                 
-                if market["id"] == "10" || "63"
+                if market["id"] == "10" || market["id"] == "63"
                     #update or create markets double chance half time and fulltime
                     market.each do |out|
                         if out["id"] == "9"
@@ -92,7 +100,7 @@ class Soccer::OddsChangeWorker
                         end
                     end
                     #update or create markets 1X2 half time and fulltime
-                    table_sym.update_or_create(event_id: event_id)  do |mkt|
+                    model_name.constantize.find_or_create_by(event_id: event_id)  do |mkt|
                         mkt.competitor1_draw =  comp1_draw_odds
                         mkt.competitior1_competitior2 = comp1_comp2_odds
                         mkt.draw_competitor2 =  draw_comp2_odds
@@ -104,7 +112,7 @@ class Soccer::OddsChangeWorker
                     
                 end
                 
-                if (market["id"] == "18" || "68") && market["specifiers"] == "total=2.5"
+                if (market["id"] == "18" || market["id"] == "68") && market["specifiers"] == "total=2.5"
                     #update or create markets under and over half time and fulltime
                     market.each do |out|
                         if out["id"] == "12"
@@ -116,7 +124,7 @@ class Soccer::OddsChangeWorker
                         
                     end
                     #update or create markets 1X2 half time and fulltime
-                    table_sym.update_or_create(event_id: event_id)  do |mkt|
+                    model_name.constantize.find_or_create_by(event_id: event_id)  do |mkt|
                         mkt.under =  under_odds
                         mkt.over = over_odds
                         mkt.threshold = 2.5
@@ -128,7 +136,7 @@ class Soccer::OddsChangeWorker
                     
                 end
                 
-                if market["id"] == "29" || "75"
+                if market["id"] == "29" || market["id"] == "75"
                     #update or create markets both to score half time and fulltime
                     market.each do |out|
                         if out["id"] == "74"
@@ -140,7 +148,7 @@ class Soccer::OddsChangeWorker
                         
                     end
                     #update or create markets 1X2 half time and fulltime
-                    table_sym.update_or_create(event_id: event_id)  do |mkt|
+                    model_name.constantize.find_or_create_by(event_id: event_id)  do |mkt|
                         mkt.yes =  yes_odds
                         mkt.no = no_odds
                         mkt.status = market_status[market["status"]]
