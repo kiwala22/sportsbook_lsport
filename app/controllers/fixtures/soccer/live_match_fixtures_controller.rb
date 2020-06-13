4f163ffa161561440b1448b19ecc6e42a6187ae6
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-06-13 13:58:47 +0300

    changed views for pre and live

--- app/controllers/fixtures/soccer/live_match_fixtures_controller.rb
+++ app/controllers/fixtures/soccer/live_match_fixtures_controller.rb
@@ -6,7 +6,7 @@ class Fixtures::Soccer::LiveMatchFixturesController < ApplicationController
   layout "admin_application.html.erb"
 
   def index
-    @fixtures = SoccerFixture.where("status = ? AND scheduled_time >= ? AND category_id != ? ","live", Time.now, "sr:category:1033").order(:scheduled_time).page params[:page]
+    @fixtures = SoccerFixture.where("status = ? AND category_id != ? ","live", "sr:category:1033").order(:scheduled_time).page params[:page]
   end
 
   def update
