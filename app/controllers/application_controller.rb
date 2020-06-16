112a00553146c10a0420fb16eb7a9c4ab81c9c11
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-06-16 19:50:59 +0300

    fixed all issues with active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
       added_attrs = [:phone_number, :email, :password, :password_confirmation, :remember_me, :first_name, :last_name]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-      #devise_parameter_sanitizer.permit(:sign_in, keys: [:phone_number])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:phone_number])
    end
 
    def set_raven_context
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,7 @@ class AdminUser < ApplicationRecord
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, 
-    :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable, authentication_keys: [:email]
     
     attr_writer :login
     
