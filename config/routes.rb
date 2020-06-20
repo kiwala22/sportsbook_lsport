befec363d93e4f43569ffa5bdc0f1e49b7e9592f
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-06-20 12:10:48 +0300

    changed routes to backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :transactions, only: [:new]
-  match 'deposit' => "transactions#deposit", via: [:post]
-  match 'resend_verify' => "verify#create", via: [:post]
-  match 'new_verify' => "verify#new", via: [:get]
-  match 'send_verification' => "verify#verify_via_email", via: [:get]
-  match 'verify' => "verify#update", via: [:put]
-   scope '/backend' do
+   resources :transactions, only: [:new]
+   match 'deposit' => "transactions#deposit", via: [:post]
+   match 'resend_verify' => "verify#create", via: [:post]
+   match 'new_verify' => "verify#new", via: [:get]
+   match 'send_verification' => "verify#verify_via_email", via: [:get]
+   match 'verify' => "verify#update", via: [:put]
+   namespace :backend do
       namespace :fixtures do
          match 'soccer_fixtures' => "soccer_fixtures#index", via: [:get]
          patch 'soccer_fixtures' => "soccer_fixtures#update", via: [:update]
@@ -20,27 +20,27 @@ Rails.application.routes.draw do
       match 'void_reasons' => "void_reasons#index", via: [:get]
       match 'match_statuses' => "match_statuses#index", via: [:get]
    end
-
+   
    namespace :amqp do
       namespace :v1  do
          match 'alerts' => 'alerts#create', via: [:post]
-
+         
          namespace :sports do
             match 'soccer' => 'soccer#create', via: [:post]
          end
       end
    end
-
+   
    devise_for :users, path: 'users',  controllers: {
       sessions: 'users/sessions'
    }
    devise_for :admins, path: 'admins',  controllers:{
       sessions: 'admins/sessions'
-
+      
    }
-
+   
    devise_scope :user do
-
+      
       authenticated :user do
          root to: 'home#index', as: :authenticated_user_root
       end
@@ -49,7 +49,7 @@ Rails.application.routes.draw do
       end
    end
    devise_scope :admin do
-
+      
       authenticated :admin do
          get '/backend' => 'admin_landing#index', as: :authenticated_admins_root
       end
@@ -60,8 +60,8 @@ Rails.application.routes.draw do
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    devise_for :admin_users, ActiveAdmin::Devise.config
    ActiveAdmin.routes(self)
-
+   
    require 'sidekiq/web'
    mount Sidekiq::Web => '/rabbit'
-
+   
 end
