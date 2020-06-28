af305ea03978687ffc418cc5bdb121a4133f90f5
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-06-29 02:36:18 +0300

    fixed fixture display

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -4,15 +4,13 @@ class FixturesController < ApplicationController
       if params[:q].present?
          @q = Fixture.ransack(params[:q])
       else
-         params[:q] = {"scheduled_time_gteq": Time.now, "scheduled_time_eq": Date.today, "status_eq": "not_started"}
+         params[:q] = {"scheduled_time_gteq": Time.now, "scheduled_time_lteq": Date.today.end_of_day, "status_eq": "not_started"}
          @q = Fixture.ransack(params[:q])
       end
       
       #.includes(:market1_pre).where("scheduled_time >= ? AND scheduled_time = ? AND status = ?", Time.now, Date.today, "not_started" )
       @fixtures = @q.result.includes(:market1_pre)
 
-      p "q: #{@q}"
-      p "fixtures: #{@fixtures}"
    end
 
    def show
