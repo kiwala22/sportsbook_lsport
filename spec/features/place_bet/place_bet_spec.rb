9682b084079fc9abce28bd8c583a2d8bdc555075
imutumba <imutumba@skylinesms.com>
2020-07-30 17:00:01 +0300

    working on place bet tests

--- spec/features/place_bet/place_bet_spec.rb
+++ spec/features/place_bet/place_bet_spec.rb
@@ -65,12 +65,31 @@ RSpec.describe User, type: :system, js: true do
 			stake = 10000
 			fill_in 'stake', with: stake
 			click_button('Place Bet')
-			expect(page).to have_content'You have insufficient balance on your account. Please deposit some money.'
-			
+			expect(page).to have_content'You have insufficient balance on your account. Please deposit some money.'			
 			
 			sleep(2)
 
 		end
 		
+		it 'should prompt login on no login session' do
+			visit '/'
+			expect(page).to have_content('Pre-Match Fixtures - Soccer')
+			expect(page).to have_content('Betsb slip')
+
+			first('#odd1').click
+			all('a', :id => 'odd1')[1].click
+			all('a', :id => 'odd2')[2].click
+			all('a', :id => 'odd3')[3].click
+			# all('a', :id => 'odd2')[5].click
+			all('a', :id => 'odd1')[4].click
+			stake = 1000
+			fill_in 'stake', with: stake
+			sleep(1)
+			
+			expect(page).to_not have_content 'Place bet'
+			click_button('Login')
+			sleep (2)
+		end
+
 	end
 end
