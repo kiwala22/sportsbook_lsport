68cf835f3b6aaba0d4e964e2688fb5b78f0d57c3
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-08-04 22:34:03 +0300

    add live soccer results spans

--- app/controllers/fixtures/virtual_soccer/live_match_controller.rb
+++ app/controllers/fixtures/virtual_soccer/live_match_controller.rb
@@ -1,19 +1,15 @@
-class Fixtures::Soccer::LiveMatchController < ApplicationController
+class Fixtures::VirtualSoccer::LiveMatchController < ApplicationController
    include CurrentCart
    before_action :set_cart, only: [:index, :show]
 
    def index
       if params[:q].present?
-         Fixture.where("status = ? AND sport_id = ? AND category_id IN (?)", "live", "sr:sport:1", ['sr:category:1033','sr:category:2123']).ransack(params[:q])
+         @q = Fixture.joins(:market1_pre).where("fixtures.status = ? AND fixtures.sport_id = ? AND fixtures.category_id NOT IN (?) AND fixtures.scheduled_time >= ? AND fixtures.scheduled_time <= ?", "live", "sr:sport:1", ["sr:category:1033","sr:category:2123"], params[:q][:start], params[:q][:stop])
       else
-         params[:q] = {"scheduled_time_gteq": Time.now, "scheduled_time_lteq": (Date.today.end_of_day + 1.day)}
-         #params[:q] = { "sport_id_eq": "sr:sport:1", "category_id_not_eq": "sr:category:1033" }
-         
-         @q = Fixture.where("status = ? AND sport_id = ? AND category_id IN (?)", "live", "sr:sport:1", ["sr:category:1033","sr:category:2123"]).ransack(params[:q])
+         @q = Fixture.joins(:market1_pre).where("fixtures.status = ? AND fixtures.sport_id = ? AND fixtures.category_id NOT IN (?) AND fixtures.scheduled_time >= ? AND fixtures.scheduled_time <= ?", "live", "sr:sport:1", ["sr:category:1033","sr:category:2123"], Time.now, Date.today.end_of_day)
       end
       
-      #.includes(:market1_pre).where("scheduled_time >= ? AND scheduled_time = ? AND status = ?", Time.now, Date.today, "not_started" )
-      @pagy, @fixtures = pagy(@q.result.includes(:market1_pre))
+      @pagy, @fixtures = pagy(@q.includes(:market1_pre))
       respond_to do |format|
          format.html
          format.js
