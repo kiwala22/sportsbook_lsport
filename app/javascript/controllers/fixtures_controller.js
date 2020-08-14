f4a6ac91c44734117d997726e72d2de54ef29f17
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-08-14 20:54:11 +0300

    auto slip counter finished

--- app/javascript/controllers/fixtures_controller.js
+++ app/javascript/controllers/fixtures_controller.js
@@ -1,12 +1,18 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+
+    static targets = []
+
+
+
     connect() {
         this.load()
 
         if (this.data.has("refreshInterval")) {
             this.startRefreshing()
         }
+
     }
 
     disconnect() {
@@ -26,15 +32,14 @@ export default class extends Controller {
     }
 
     load() {
-        let url = "/refresh_slip"
+        let url = this.data.get("url")
         Rails.ajax({
-            type: 'POST',
+            type: 'GET',
             url: url,
             dataType: 'js',
             success: (data) => {
-                console.log("Success")
+                console.log(data)
             }
         })
     }
-
 }
---
+++ app/javascript/controllers/slips_controller.js
@@ -0,0 +1,62 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+    static targets = ["odd", "wins", "total", "stake"]
+
+    connect() {
+        this.calculate_odds()
+        if (localStorage.getItem("stake") !== null) {
+            this.display_win()
+        } else {
+            $("#stake-input").val("")
+        }
+
+    }
+
+    calculate_odds() {
+        var oddsArr = new Array();
+        if (this.hasOddTarget) {
+            this.oddTargets.forEach(element => {
+                oddsArr.push(element.innerHTML)
+            });
+        }
+        oddsArr = oddsArr.map(Number);
+        var total = this.multiply_odds(oddsArr)
+
+        this.totalTarget.innerHTML = total
+        let count = document.getElementById('slip-ticket-count')
+        count.innerHTML = oddsArr.length
+
+
+    }
+
+    multiply_odds(arr) {
+        var product = 1
+        for (var i = 0; i < arr.length; i++) {
+            product = product * arr[i]
+        }
+        return product.toFixed(2)
+    }
+
+    calculate_win() {
+        const amount = event.target.value
+        localStorage.setItem("stake", amount)
+        const totalOdd = parseFloat(this.totalTarget.innerHTML)
+        this.winsTarget.innerHTML = 'UGX ' + (totalOdd * amount).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    }
+
+    display_win() {
+        if (localStorage.getItem("stake") !== 'null') {
+            let stakeAmount = parseFloat(localStorage.getItem("stake"))
+
+            if (stakeAmount !== "null") {
+                $("#stake-input").val(stakeAmount)
+                const totalOdd = parseFloat(this.totalTarget.innerHTML)
+                this.winsTarget.innerHTML = 'UGX ' + (totalOdd * stakeAmount).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            }
+
+        }
+
+    }
+
+}
