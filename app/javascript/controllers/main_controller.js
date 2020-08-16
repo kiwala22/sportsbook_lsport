8ce8cf62931413fa609fe506109fecbdb46863a0
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-08-16 06:02:34 +0300

    finished mobile slip pending refreshing every 30 seconds

---
+++ app/javascript/controllers/main_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+    static targets = ["betslip"]
+
+    closeSlip() {
+        this.betslipTarget.classList.remove("active")
+        console.log(this.betslipTarget.classList)
+    }
+
+    showSlip() {
+        this.betslipTarget.classList.toggle("active")
+            //   console.log(this.betslipTarget.classList)
+    }
+
+}
