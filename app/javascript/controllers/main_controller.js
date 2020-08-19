28ff9f3bf021a8469a5d4656aeec3b8030c88a88
martin kiwala <martinkiwala4@gmail.com>
2020-08-19 18:04:10 +0300

    multiple changes in views

--- app/javascript/controllers/main_controller.js
+++ app/javascript/controllers/main_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
 
     showSlip() {
         this.betslipTarget.classList.toggle("active")
-            //   console.log(this.betslipTarget.classList)
+            //console.log(this.betslipTarget.classList)
     }
 
 }
