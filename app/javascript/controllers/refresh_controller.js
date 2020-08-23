6403df8ff0856b26e773d97768e5ec0a3df3e48b
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-08-23 22:03:11 +0300

    log refresh

--- app/javascript/controllers/refresh_controller.js
+++ app/javascript/controllers/refresh_controller.js
@@ -39,7 +39,7 @@ export default class extends Controller {
             url: url,
             dataType: 'js',
             success: (data) => {
-
+                console.log("Success - " + url)
             }
         })
     }
