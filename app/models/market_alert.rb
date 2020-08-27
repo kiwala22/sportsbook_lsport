dcbfbbfe47f4fbe438422b0f99f28c5f820723ee
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-08-27 22:10:31 +0300

    changed check producer to an instance method

--- app/models/market_alert.rb
+++ app/models/market_alert.rb
@@ -1,8 +1,9 @@
 class MarketAlert < ApplicationRecord
    
    include Recovery
+   include Betradar
    
-   def self.check_producers
+   def check_producers
       ["1", "3"].each do |product|
          last_update = MarketAlert.where(:product => product, subscribed: "1").order("timestamp DESC").first
          if last_update
