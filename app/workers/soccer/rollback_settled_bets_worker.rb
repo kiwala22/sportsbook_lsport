a284817c15a046030a86f8683a5f05519bc08cdb
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-08-28 16:30:12 +0300

    fix bet status and result on games

--- app/workers/soccer/rollback_settled_bets_worker.rb
+++ app/workers/soccer/rollback_settled_bets_worker.rb
@@ -8,7 +8,7 @@ class Soccer::RollbackSettledBetsWorker
    def perform(fixture_id, product, market_id)
       #find the fixture
       fixture = Fixture.find(fixture_id)
-      bets = fixture.bets.where(product: product, market_id: market_id, status: ["Win", "Loss"])
+      bets = fixture.bets.where(product: product, market_id: market_id, result: ["Win", "Loss"])
       
       if bets
          bets.each do |bet|
