b04ef2ac2bd55bf664c89491a82da61a3146b5a4
imutumba <imutumba@skylinesms.com>
2020-09-05 19:00:43 +0300

    search with rails:ajax

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,58 +1,37 @@
 import {Controller} from "stimulus"
 
 export default class extends Controller {
-	static targets = ["q", "fixtures"]
+	static targets = ["search", "fixtures"]
 
 
 	connect(){
-		// this.searchFixture();
-		this.searchFixture();
+		
+		document.getElementById("fixture-table-body").removeAttribute("data-controller");
+		// document.getElementById("fixture-table-body").removeAttribute("data-target");
+		document.getElementById("fixture-table-body").removeAttribute("data-refresh-interval");
+		// document.getElementById("paginate").removeAttribute("data-target");
+		document.getElementById("spinner").remove();
+		
 	}
 
 	searchFixture(){
-		try{
+		
 		const matchList = document.getElementById('fixture-table-body')
 		const search = document.getElementById('search')
-		const value =  this.qTarget.value
-		fetch(`/?q=${value}`, {
-			headers: {
-				accept: 'application/json'
-			}
-		}).then((response) => response.json())
-		.then( data => {
-			var fixtureArray = Object.values(data)[0]
+		const value =  this.searchTarget.value
+		let token = document.getElementsByName('csrf-token')[0].content
+		let next_page = null
+		let url = next_page.href
 
-			if(search.value.length>0){
-
-			//***check for elements in fixture array
-				if(fixtureArray !==[]){
-					matchList.innerHTML = fixtureArray
-
-				//***removing the refresh controller
-					document.getElementById("fixture-table-body").removeAttribute("data-controller");
-					document.getElementById("fixture-table-body").removeAttribute("data-refresh-interval");
-
-
-				//***removing pagination & bottom spinner
-					var bottom = document.getElementById("bottom");
-					bottom.remove()
-				
-					throw new Error('removed temporarilly')
-				}else{
-					matchList.innerHTML = "No Fixtures"
-				}
-
-			}else{
-				matchList.innerHTML = []
-			}
-
-		})
-	}catch(e){
-			console.error("Occured:"+e.message);
-			return []
-		}
-					
+			Rails.ajax({
+            type: 'GET',
+            headers: {
+                'X-CSRF-Token': token
+            },
+            url:url,
+            dataType: 'json',
+            success: (data) => {}
+        
+		})					
 	}
-
-
 }
