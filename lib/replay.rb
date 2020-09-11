48b707351567426eb193ebe1a803054e49d16b57
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-09-11 22:20:11 +0300

    added csv and replay lib

---
+++ lib/replay.rb
@@ -0,0 +1,63 @@
+module Replay
+   require 'openssl'
+   require 'json'
+   require 'uri'
+   require 'net/http'
+   require 'logger'
+
+   @@logger ||= Logger.new("#{Rails.root}/log/betradar.log")
+   @@logger.level = Logger::ERROR
+
+   @@end_point = "https://stgapi.betradar.com/v1/"
+   @@auth_token = 's5X0wAgEmHCxDqrPnI'
+   include Betradar
+   def load_fixtures
+      CSV.foreach("#{Rails.root}/public/soccer.csv", headers: true) do |row|
+         
+         #create the fixtures
+         response = fetch_fixture("sr:match:#{row[0]}")
+
+         event = Hash.from_xml(response)
+         puts event
+         
+         case event["fixtures_fixture"]["fixture"]["tournament"]["sport"]["id"]
+         when "sr:sport:1"
+            Soccer::CreateFixtureWorker.perform_async(event["fixtures_fixture"]["fixture"])
+         end
+
+         sleep 1
+      end
+   end
+
+   def add_to_replay
+      CSV.foreach("#{Rails.root}/public/soccer.csv", headers: true) do |row|
+         #add to replay
+         add_event("sr:match:#{row[0]}")
+      end
+   end
+
+   def add_event(event_id)
+      url = @@end_point + "replay/events/#{event_id}"
+      uri = URI(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.read_timeout = 180
+      request = Net::HTTP::Put.new(uri.request_uri)
+      request['x-access-token'] = @@auth_token
+      http.use_ssl = true
+      http.ssl_version = 'TLSv1'
+      #http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.set_debug_output($stdout)
+      response = http.request(request)
+      event = Hash.from_xml(response.body)
+      
+      if response.code == "200"
+         return 200
+      else
+         @@logger.error(response.body)
+         return 400
+      end
+   end
+   
+   
+   
+end
