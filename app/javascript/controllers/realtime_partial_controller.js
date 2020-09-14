b3a57b211d7c849c1a1a82889a35cb23096e47f6
martin kiwala <martinkiwala4@gmail.com>
2020-09-14 10:10:37 +0300

    testing JS file if being executed

--- app/javascript/controllers/realtime_partial_controller.js
+++ app/javascript/controllers/realtime_partial_controller.js
@@ -12,7 +12,9 @@ export default class extends Controller {
       },
       {
         received({ market, fixture }) {
+          console.log("Data received");
           market ? self.onMarketChange(market) : self.onFixtureChange(fixture);
+          console.log("method executed");
         }
       }
     );
