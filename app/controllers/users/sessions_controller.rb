b4b188c93fedde2c71f565edaa7e265c9a55fc20
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-09-16 04:02:40 +0300

    remove authenticity requirment on session create

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  skip_before_filter :verify_authenticity_token, :only => :create
   layout "application"
   include CurrentCart
   before_action :set_cart
