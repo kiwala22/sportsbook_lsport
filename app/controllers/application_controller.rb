b78d58eb4a1b13439658163a637e77e68b900d71
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-09-21 17:06:06 +0300

    add protect from forgery to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception, prepend: true
   include Pagy::Backend
 
   before_action :configure_permitted_parameters, if: :devise_controller?
