4701f5094dd28dc9f6177873d4b37ad3b78eb9ce
martin kiwala <martinkiwala4@gmail.com>
2020-09-21 21:15:02 +0300

    initial tests on odds changes, untested yet due to DB restriction changes

--- lib/tasks/fixtures.rake
+++ lib/tasks/fixtures.rake
@@ -3,7 +3,7 @@ task pull_fixture: :environment do
    date_today = Date.today.strftime("%F")
    date_one = (Date.today + 1.day).strftime("%F")
    date_two = (Date.today + 2.day).strftime("%F")
-   date_three = (Date.today + 2.day).strftime("%F")
+   date_three = (Date.today + 3.day).strftime("%F")
 
    fixture_today = Fixture.new.fetch_fixtures(date_today)
    fixture_one =   Fixture.new.fetch_fixtures(date_one)
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "action_cable/testing/rspec"
+require "rspec/rails/shared_contexts/action_cable"
 require 'rspec/rails'
 require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -34,12 +36,12 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  #config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
