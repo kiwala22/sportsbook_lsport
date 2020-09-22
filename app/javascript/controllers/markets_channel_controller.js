b6ccbd0671cf204076c6ee05bb530d3bc227701b
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-09-22 03:32:56 +0300

    fixed time and score, now tracing refresh

--- app/javascript/controllers/markets_channel_controller.js
+++ app/javascript/controllers/markets_channel_controller.js
@@ -10,12 +10,12 @@ export default class extends Controller {
     }
 
     refresh_fixture() {
-        console.log(this.data.get("url"), this.get("method"));
+        console.log(this.data.get("url"), this.data.get("method"));
         if (this.data.has("url") && this.data.has("method")) {
             let url = this.data.get("url");
             let method = this.data.get("method");
             let token = document.getElementsByName('csrf-token')[0].content;
-            console.log(url, method)
+            console.log(url, method);
             $("#fixture-table-body-1").empty();
             $("#fixture-table-body").empty();
             $("#fixture-table-body-1").append('<div class="d-flex justify-content-center"><div class="spinner-border" role="status"></div></div>');
@@ -32,7 +32,7 @@ export default class extends Controller {
                 success: (data) => {
                     $("#bottom").show();
                 }
-            })
+            });
         }
 
 
