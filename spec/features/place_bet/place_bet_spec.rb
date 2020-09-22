7f916021f0f8193a3bf84f1c45aaebea40bd5cde
imutumba <imutumba@skylinesms.com>
2020-09-22 08:11:06 +0000

    place_bet_spec update

--- spec/features/place_bet/place_bet_spec.rb
+++ spec/features/place_bet/place_bet_spec.rb
@@ -60,46 +60,46 @@ RSpec.describe User, type: :system, js: true do
 		end
 
 
-		# it 'should fail on low balance' do
-		# 	login_form(user.phone_number, user.password)
-		# 	expect(page).to have_content('Upcoming Fixtures - Soccer')
-		# 	expect(page).to have_content('Betslip')
-		# 	first("#pre_1_#{fixture.id}").click
-		# 	# all('a', :id => 'odd1')[1].click
-		# 	# all('a', :id => 'odd2')[2].click
-		# 	# all('a', :id => 'odd3')[3].click
-		# 	# all('a', :id => 'odd2')[5].click
-		# 	# all('a', :id => 'odd1')[4].click
-		# 	stake = 20000
-		# 	fill_in 'stake', with: stake
-		# 	# expect('total-odds').to eq()
-		# 	expect('total-wins'.to_f).to eq('total-odds'.to_f * 'stake-input'.to_i)
-		# 	 expect{
-		# 		click_button('Place Bet')
-		# 	 }.to change(BetSlip, :count).by(0)					
-		# 	expect(page).to have_content'You have insufficient balance on your account. Please deposit some money.'	
-		# end
+		 it 'should fail on low balance' do
+			login_form(user.phone_number, user.password)
+		 	expect(page).to have_content('Upcoming Fixtures - Soccer')
+		 	expect(page).to have_content('Betslip')
+		 	first("#pre_1_#{fixture.id}").click
+		 	# all('a', :id => 'odd1')[1].click
+		 	# all('a', :id => 'odd2')[2].click
+		 	# all('a', :id => 'odd3')[3].click
+		 	# all('a', :id => 'odd2')[5].click
+		 	# all('a', :id => 'odd1')[4].click
+		 	stake = 20000
+		 	fill_in 'stake', with: stake
+		 	# expect('total-odds').to eq()
+		 	expect('total-wins'.to_f).to eq('total-odds'.to_f * 'stake-input'.to_i)
+		 	 expect{
+		 		click_button('Place Bet')
+		 	 }.to change(BetSlip, :count).by(0)					
+		 	expect(page).to have_content'You have insufficient balance on your account. Please deposit some money.'	
+		 end
 
 		
-		# it 'User can login after choosing games' do
-		# 	visit '/'
-		# 	expect(page).to have_content('Upcoming Fixtures - Soccer')
-		# 	expect(page).to have_content('Betslip')
-		# 	first("#pre_3_#{fixture.id}").click
-		# 	# all('a', :id => 'odd1')[1].click
-		# 	# all('a', :id => 'odd2')[2].click
-		# 	# all('a', :id => 'odd3')[3].click
-		# 	# all('a', :id => 'odd2')[5].click
-		# 	# all('a', :id => 'odd1')[4].click
-		# 	stake = 2000
-		# 	fill_in 'stake', with: stake
-		# 	expect('total-wins'.to_f).to eq('total-odds'.to_f * 'stake-input'.to_i)
-		# 	sleep(1)		
-		# 	expect(page).to_not have_content 'Place bet'
-		# 	click_link('slip_login')
-		# 	login_form(user.phone_number, user.password)
-		# 	sleep (1)
-		# end
+		 it 'User can login after choosing games' do
+		 	visit '/'
+		 	expect(page).to have_content('Upcoming Fixtures - Soccer')
+		 	expect(page).to have_content('Betslip')
+		 	first("#pre_3_#{fixture.id}").click
+		 	# all('a', :id => 'odd1')[1].click
+		 	# all('a', :id => 'odd2')[2].click
+		 	# all('a', :id => 'odd3')[3].click
+		 	# all('a', :id => 'odd2')[5].click
+		 	# all('a', :id => 'odd1')[4].click
+		 	stake = 2000
+		 	fill_in 'stake', with: stake
+		 	expect('total-wins'.to_f).to eq('total-odds'.to_f * 'stake-input'.to_i)
+		 	sleep(1)		
+		 	expect(page).to_not have_content 'Place bet'
+		 	click_link('slip_login')
+		 	login_form(user.phone_number, user.password)
+			sleep (1)
+		 end
 
 	end
 end
