79e01a45ee343db67f62d5d29c09878024330e51
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-09-22 05:16:26 +0300

    added reload for markets views

--- app/javascript/controllers/markets_channel_controller.js
+++ app/javascript/controllers/markets_channel_controller.js
@@ -7,11 +7,30 @@ export default class extends Controller {
         this.subscription = consumer.subscriptions.create({
             channel: "MarketsChannel",
             fixture: this.data.get("fixture")
-        }, { received: this.refresh_fixture() });
+        }, { received: this.refresh_fixture(this.data.get("url"), this.data.get("method")) });
     }
 
-    refresh_fixture() {
-        location.reload()
+    refresh_fixture(url, method) {
+
+        let token = document.getElementsByName('csrf-token')[0].content;
+        console.log(url, method);
+        $("#fixture-table-body-1").empty();
+        $("#fixture-table-body").empty();
+        $("#fixture-table-body-1").append('<div class="d-flex justify-content-center"><div class="spinner-border" role="status"></div></div>');
+        $("#fixture-table-body").append('<div class="d-flex justify-content-center"><div class="spinner-border" role="status"></div></div>');
+        $("#bottom").hide();
+
+        Rails.ajax({
+            type: method,
+            headers: {
+                'X-CSRF-Token': token
+            },
+            url: url,
+            dataType: 'script',
+            success: (data) => {
+                $("#bottom").show();
+            }
+        });
 
     }
 
