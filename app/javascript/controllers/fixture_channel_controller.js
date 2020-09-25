8a809d31a920f45bcf38f202e7d4bda18d95b07e
martin kiwala <martinkiwala4@gmail.com>
2020-09-25 16:41:47 +0300

    reduced logs to market 1 and 29 in soccer controller and removed all console logs in JS files

--- app/javascript/controllers/fixture_channel_controller.js
+++ app/javascript/controllers/fixture_channel_controller.js
@@ -4,7 +4,6 @@ import $ from 'jquery'
 
 export default class extends Controller {
     connect() {
-        console.log("Fixtures connected");
         this.subscription = consumer.subscriptions.create({
             channel: "FixtureChannel",
             fixture: this.data.get("fixture")
@@ -15,7 +14,6 @@ export default class extends Controller {
 
 
     update_fixture(data) {
-        console.log("Fixtures called");
         var match_time = `#match_time_${data.id}`;
         var match_score = `#match_score_${data.id}`;
         //update the time and score
