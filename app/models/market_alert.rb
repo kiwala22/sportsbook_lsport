f2b2730c08354a79bfc28eac9aaa22ded10f9e06
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-09-28 16:27:19 +0300

    added restart script

--- app/models/market_alert.rb
+++ app/models/market_alert.rb
@@ -10,6 +10,9 @@ class MarketAlert < ApplicationRecord
             if ((Time.now.to_i * 1000) - last_update[:timestamp].to_i) > 20000
                #first close all active markets 
                DeactivateMarketsWorker.perform_async(product)   
+
+               ##attempt manual activation of betradar amqp
+               puts %x{systemctl restart betradar}
             end
          end
       end
