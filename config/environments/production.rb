f83ac8d7736ceb2e8f24a6ed0858bd8e4f672934
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-10-01 11:57:45 +0300

    chanded cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,8 @@
 Rails.application.configure do
   # Specify AnyCable WebSocket server URL to use by JS client
-  #commented out 1/10/20 - Acacia
-  # config.after_initialize do
-  #   config.action_cable.url = ActionCable.server.config.url = ENV.fetch("CABLE_URL") if AnyCable::Rails.enabled?
-  # end
+  config.after_initialize do
+    config.action_cable.url = ActionCable.server.config.url = ENV.fetch("CABLE_URL") if AnyCable::Rails.enabled?
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
