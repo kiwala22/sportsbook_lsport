5eb70bf0b9c7d5b5c4607c6578830af09ff2f39f
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-10-02 00:42:26 +0300

    reverted to default settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,8 @@
 Rails.application.configure do
-  config.action_cable.url = "ws://localhost:3334/cable"
-  config.action_cable.allowed_request_origins = ['http://localhost:3000']
-  
+  # Specify AnyCable WebSocket server URL to use by JS client
+  config.after_initialize do
+    config.action_cable.url = ActionCable.server.config.url = ENV.fetch("CABLE_URL", "ws://localhost:8080/cable") if AnyCable::Rails.enabled?
+  end
   
   # Settings specified here will take precedence over those in config/application.rb.
   
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
-  config.action_cable.url = "ws://104.248.165.210:6565/cable"
-  config.action_cable.allowed_request_origins = ['http://104.248.165.210']
+  # Specify AnyCable WebSocket server URL to use by JS client
+  config.after_initialize do
+    config.action_cable.url = ActionCable.server.config.url = ENV.fetch("CABLE_URL") if AnyCable::Rails.enabled?
+  end
   
   # Settings specified here will take precedence over those in config/application.rb.
   
