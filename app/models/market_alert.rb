e5f286caaad2957e88eaf1a302c8feaccb59e42a
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-10-04 01:13:02 +0300

    added connection attempts every 10 seconds

--- app/models/market_alert.rb
+++ app/models/market_alert.rb
@@ -4,21 +4,25 @@ class MarketAlert < ApplicationRecord
    include Betradar
    
    def check_producers
-      ["1", "3"].each do |product|
-         last_update = MarketAlert.where(:product => product, subscribed: "1").order("timestamp DESC").first
-         if last_update
-            if ((Time.now.to_i * 1000) - last_update[:timestamp].to_i) > 20000
-               #first close all active markets 
-               DeactivateMarketsWorker.perform_async(product)   
+      (0..5).each do 
+         ["1", "3"].each do |product|
+            last_update = MarketAlert.where(:product => product, subscribed: "1").order("timestamp DESC").first
+            if last_update
+               if ((Time.now.to_i * 1000) - last_update[:timestamp].to_i) > 15000
+                  #first close all active markets 
+                  DeactivateMarketsWorker.perform_async(product)   
+               end
             end
          end
-      end
 
-      #check if connection is down
-      last_alert = MarketAlert.last
-      if last_alert && (((Time.now.to_i * 1000) - last_alert[:timestamp].to_i) > 60000)
-         #if the last update irrespective if product is more than 60 seconds ago, then manual restart
-         system('systemctl restart betradar')
+         #check if connection is down
+         last_alert = MarketAlert.last
+         if last_alert && (((Time.now.to_i * 1000) - last_alert[:timestamp].to_i) > 15000)
+            #if the last update irrespective if product is more than 60 seconds ago, then manual restart
+            system('systemctl restart betradar')
+         end
+
+         sleep 10
       end
    end
    
