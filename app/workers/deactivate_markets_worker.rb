81b6c577c194724de0a7b44fd62d937fd42f635d
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-10-05 12:07:16 +0300

    reverted to old market deactivation and betstop

--- app/workers/deactivate_markets_worker.rb
+++ app/workers/deactivate_markets_worker.rb
@@ -5,21 +5,19 @@ class DeactivateMarketsWorker
    sidekiq_options retry: false
    
    def perform(product)
-      threads = []
       producer_type = {
          "1" => "Live",
          "3" => "Pre"
       }
       markets = ["1","60", "10", "63", "18","68", "29", "75", "16", "66"]
-      #check all markets in parallel  
+      
       markets.each do |market|
-         threads << Thread.new do
-            model_name = "Market" + market + producer_type[product]
-            #deactivate all active markets
-            model_name.constantize.where(status: "Active").update_all(status: "Deactivated")
+         model_name = "Market" + market + producer_type[product]
+         entries = model_name.constantize.where(status: "Active")
+         if entries
+            entries.update(status: "Deactivated")
          end
       end
-      threads.each { |thr| thr.join }
    end
    
 end
