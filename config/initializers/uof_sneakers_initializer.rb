4881316cd500b93f8ec99d0256e94d6a4e82d741
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-10-10 19:32:47 +0300

    changed to sneakers

--- config/initializers/uof_sneakers_initializer.rb
+++ config/initializers/uof_sneakers_initializer.rb
@@ -1,20 +1,19 @@
 require "sneakers"
 begin
    Sneakers.configure   :heartbeat => 5,
-                        :amqp => 'amqp://s5X0wAgEmHCxDqrPnI@stgmq.betradar.com:5671',
+                        :amqp => 'amqp://s5X0wAgEmHCxDqrPnI@replaymq.betradar.com:5671',
                         :vhost => '/unifiedfeed/30819',
                         :log  => 'log/betradar.log',     # Log file
-                        :pid_path => 'tmp/pids//sneakers.pid', # Pid file
+                        :pid_path => 'tmp/pids/sneakers.pid', # Pid file
                         :tls => true,
                         :verify_peer => false,
                         :verify_peer_name => false,
                         :allow_self_signed => true,
                         :workers => 2
-
-
+   
    Sneakers.logger = Rails.logger
    Sneakers.logger.level = Logger::INFO
 rescue Exception => e
    # bad, but otherwise precompile assets blow'allow_self_signed' => trues up in Docker
    Rails.logger.error(e.message)
- end
+end
