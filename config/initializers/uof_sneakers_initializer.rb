b466a05b075594cd6cb53fbf4c5845dfab2f41a5
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-10-11 01:52:42 +0300

    changed sneakers logger

--- config/initializers/uof_sneakers_initializer.rb
+++ config/initializers/uof_sneakers_initializer.rb
@@ -3,7 +3,7 @@ begin
    Sneakers.configure   :heartbeat => 5,
                         :amqp => 'amqp://s5X0wAgEmHCxDqrPnI@replaymq.betradar.com:5671',
                         :vhost => '/unifiedfeed/30819',
-                        :log  => Rails.logger,     # Log file
+                        :log  => 'log/sneakers.log',     # Log file
                         :pid_path => 'tmp/pids/sneakers.pid', # Pid file
                         :tls => true,
                         :verify_peer => false,
