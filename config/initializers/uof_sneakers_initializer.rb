549dbfb89f5459d725edf7ad519dd6054cf18ae9
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-11-02 13:03:05 +0300

    changed to production token

--- config/initializers/uof_sneakers_initializer.rb
+++ config/initializers/uof_sneakers_initializer.rb
@@ -1,7 +1,7 @@
 require "sneakers"
 begin
    Sneakers.configure   :heartbeat => 5,
-                        :amqp => 'amqp://s5X0wAgEmHCxDqrPnI@stgmq.betradar.com:5671',
+                        :amqp => 'amqp://ANRL2tQf8N40oGQ4Ye@mq.betradar.com:5671',
                         :vhost => '/unifiedfeed/30819',
                         # :log  => '/var/www/html/sportsbook/shared/log/sneakers.log',     # Log file #removed to let systemed manage its own logging
                         :tls => true,
