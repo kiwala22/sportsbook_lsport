35fbcfb670536bf2925574bbe88dcf7c52ad2a89
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2021-06-16 11:12:20 +0300

    changed check producers

--- app/models/market_alert.rb
+++ app/models/market_alert.rb
@@ -8,7 +8,7 @@ class MarketAlert < ApplicationRecord
          ["1", "3"].each do |product|
             last_update = MarketAlert.where(:product => product).order("timestamp DESC").first
             if last_update
-               if ((Time.now.to_i ) - last_update[:timestamp].to_i) > 15
+               if ((Time.now.to_i ) - last_update[:timestamp].to_i) > 20
                   #first close all active markets 
                   DeactivateMarketsWorker.perform_async(product)  
 
@@ -16,7 +16,7 @@ class MarketAlert < ApplicationRecord
                   #try and activate the markets again
                   #issue recovery API call
                   recovery = request_recovery(product, last_update[:timestamp])  
-                  if recovery == "20"
+                  if recovery == "200"
                      recovery_status = true
                   end 
                end
@@ -27,7 +27,7 @@ class MarketAlert < ApplicationRecord
          last_alert = MarketAlert.last
          if last_alert && (((Time.now.to_i ) - last_alert[:timestamp].to_i) > 60)
             #if the last update irrespective if product is more than 60 seconds ago, then manual restart
-            system('systemctl restart prematch-sneakers && systemctl restart inplay-sneakers && systemctl restart lsport-inplay-lsport_inplay.1.service && systemctl restart lsport-prematch-lsport_prematch.1.service')
+            system('systemctl restart sneakers && systemctl restart lsport-inplay-lsport_inplay.1.service && systemctl restart lsport-prematch-lsport_prematch.1.service')
             
          end
    
