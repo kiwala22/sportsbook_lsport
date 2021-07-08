4f5ef7f52127c97c63123b76900825122258fa64
martin kiwala <martinkiwala4@gmail.com>
2021-07-08 15:13:19 +0300

    more improvements more so on mobile betslip

--- app/javascript/controllers/main_controller.js
+++ app/javascript/controllers/main_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
     static targets = ["betslip"]
 
     connect() {
-        this.checkNumberOfGames()
+        this.hideCloseButton()
         if (this.data.has("interval")) {
             this.startRefreshing();
         }
@@ -17,7 +17,7 @@ export default class extends Controller {
 
     startRefreshing() {
         this.refreshTimer = setInterval(() => {
-            this.checkNumberOfGames();
+            this.hideCloseButton();
         }, this.data.get("interval"))
     }
 
@@ -43,22 +43,67 @@ export default class extends Controller {
     }
 
     removeBet(e) {
-        $(e.target.closest('.lineBet')).hide()
+        $(e.target.closest('.lineBet')).remove()
+        var games = this.checkNumberOfGames();
+        $('#slip-ticket-count').html(`${games}`);
+        this.clearBetSlip();
+    }
+
+    clearBetSlip() {
+        if ($('.lineBet').length == 0){
+            $('#myModalBody').html('<p class="d-flex justify-content-center">BetSlip Cleared</p>');
+            setTimeout(() => {
+                $('#myModalBody').empty();
+            }, 2000);
+        }else {
+            let odds = [];
+            let myDiv = $('.lineBet');
+            for (let index = 0; index < myDiv.length; index++) {
+                const lineBet = $(myDiv[index]);
+                let singleBet = lineBet.find('.single-bet')
+                let odd = parseFloat(singleBet.children().last().html())
+                odds.push(odd);
+            }
+            this.calculateOdds(odds);
+        }
+    }
+
+    calculateOdds(arr) {
+        let newOdds = (arr.reduce((a, b) => a * b, 1)).toFixed(2);
+        $('#total-odds').html(`${newOdds}`);
+
+        if ($('#stake-input').val() !== null && ($('#stake-input').val() >= 1000 || $('#stake-input').val() <= 1000000)) {
+            let newAmount = ($('#stake-input').val() * newOdds).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            $('#total-wins').html(`UGX ${newAmount}`);
+        }else {
+            $('#total-wins').html('');
+        }
     }
 
     checkNumberOfGames() {
-        Rails.ajax({
-            type: 'get',
-            url: '/button_display',
-            dataType: 'json',
-            success: (data) => {
-                var total = data.games
-                if (total == 0) {
-                    $("#close-button").hide()
-                }else if (total > 0) {
-                    $("#close-button").show()
-                }
-            }});
+        var games = $('.lineBet').length;
+        // return new Promise((resolve, reject) => {
+        //     Rails.ajax({
+        //         type: 'get',
+        //         url: '/button_display',
+        //         dataType: 'json',
+        //         success: (data) => {
+        //             resolve(data.games);
+        //         }
+        //     });
+        // });
+        return games;
+    }
+
+    hideCloseButton() {
+        var num = this.checkNumberOfGames();
+        if (num == 0) {
+            $("#close-button").hide()
+            $("#betslip").hide()
+        }else if (num > 0) {
+            $("#close-button").show()
+            $("#betslip").show()
+        }
     }
 
 }
