ab59d65b9e29d3cd9a50d55c0b2104fe5b5a7d92
imutumba <imutumba@skylinesms.com>
2021-10-05 21:33:36 +0300

    fixed live match next line problems and css for search

--- app/javascript/src/components/LiveVirtualMatches.jsx
+++ app/javascript/src/components/LiveVirtualMatches.jsx
@@ -3,7 +3,7 @@ import { Table } from "antd";
 import "channels";
 import cogoToast from "cogo-toast";
 import React, { useEffect, useState } from "react";
-import { BsDash } from "react-icons/bs";
+// import { BsDash } from "react-icons/bs";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import shortUUID from "short-uuid";
@@ -89,7 +89,7 @@ const LiveVirtualMatches = (props) => {
               </strong>
               <strong>
                 <span className="score">
-                  {fixture.home_score} <BsDash /> {fixture.away_score}
+                  {fixture.home_score} - {fixture.away_score}
                 </span>
               </strong>
             </a>
