464270d5e90e7d5b2da07aff588f93953bfae5bd
imutumba <imutumba@skylinesms.com>
2021-10-13 15:08:56 +0300

    changed time format

--- app/javascript/src/components/Home.jsx
+++ app/javascript/src/components/Home.jsx
@@ -211,7 +211,7 @@ const Home = (props) => {
               {date}
             </Moment>
             <br />
-            <Moment format="MMM/DD">{date}</Moment>
+            <Moment format="DD-MMM">{date}</Moment>
             
           </a>
         </>
@@ -319,7 +319,7 @@ const Home = (props) => {
               {date}
             </Moment>
             <br />
-            <Moment format="MMM/DD">{date}</Moment>
+            <Moment format="DD-MMM">{date}</Moment>
           </a>
         </>
       ),
