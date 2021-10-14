11ac4092637334ce99fc3172b3fea98ec5928985
martin kiwala <martinkiwala4@gmail.com>
2021-10-14 16:28:24 +0300

    fixed syntax error in helper

--- app/helpers/betslip_cart_helper.rb
+++ app/helpers/betslip_cart_helper.rb
@@ -1,11 +1,11 @@
 module BetslipCartHelper
    
-   def fetch_market_status(market, identifier, fixture_id, specifier: nil)
+   def fetch_market_status(market, identifier, fixture_id, specifier = nil)
       status = market.constantize.find_by(fixture_id: fixture_id, market_identifier: identifier, specifier: specifier).status
       return status
    end
    
-   def fetch_current_odd(market, identifier, fixture_id, outcome, specifier: nil)
+   def fetch_current_odd(market, identifier, fixture_id, outcome, specifier = nil)
       odd = 1.0
       oddValue = market.constantize.find_by(fixture_id: fixture_id, market_identifier: identifier, specifier: specifier).send("odds")["outcome_#{outcome}"]
       odd = oddValue.to_f.round(2) unless oddValue.nil?
