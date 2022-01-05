fee6df026a8c7a3d394f68bddf239e70f87ae295
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2022-01-05 15:58:13 +0300

    changed the threshold

--- app/models/market_alert.rb
+++ app/models/market_alert.rb
@@ -22,9 +22,16 @@ class MarketAlert < ApplicationRecord
 	def check_producers
 		(0..5).each do 
 			["1", "3"].each do |product|
+				if product == "3"
+		            threshold = 60
+		        end
+
+		        if product == "1"
+		            threshold = 20
+		        end
 				last_update = MarketAlert.where(:product => product).last
 				if last_update
-					if ((Time.now.to_i ) - last_update[:timestamp].to_i) > 20
+					if ((Time.now.to_i ) - last_update[:timestamp].to_i) > threshold
 				  		#first close all active markets 
 				  		DeactivateMarketsWorker.perform_async(product)  
 
