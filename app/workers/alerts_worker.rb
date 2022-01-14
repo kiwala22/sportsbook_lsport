81617fc42bf3cb1f44bc28230b2e383cd1a7ead9
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2022-01-14 20:04:31 +0300

    switched to puts

--- app/workers/alerts_worker.rb
+++ app/workers/alerts_worker.rb
@@ -32,7 +32,7 @@ class AlertsWorker
         else
             
             if (timestamp.to_i - last_update[:timestamp].to_i) > threshold
-                puts "Deactivation::::::: timestamp: #{timestamp}, new stamp: #{last_update[:timestamp]}, product: #{product}"
+                puts "Deactivation :: WORKERS ::::: timestamp: #{timestamp}, new stamp: #{last_update[:timestamp]}, product: #{product}"
                 #first close all active markets 
                 DeactivateMarketsWorker.perform_async(product)
 
